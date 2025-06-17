0260ad190c91e115cdf1ab7037ce3905352649de
Rayhan Ahmed <33114212@students.uwl.ac.uk>
2025-06-17 15:08:49 +0100

    Initial commit

---
+++ IqamahTests/IqamahTests.swift
@@ -0,0 +1,17 @@
+//
+//  IqamahTests.swift
+//  IqamahTests
+//
+//  Created by R Ahmed on 08/04/2025.
+//
+
+import Testing
+@testable import Iqamah
+
+struct IqamahTests {
+
+    @Test func example() async throws {
+        
+    }
+
+}
